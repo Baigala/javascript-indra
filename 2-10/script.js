c1eff5158c77a3b84f57efdaf14df80eb5871475
baigal <osmply@gmail.com>
2022-02-11 23:52:22 -0800

    first commit

---
+++ 2-10/script.js
@@ -0,0 +1,20 @@
+// node -v
+// npm -v
+// ls, windows(dir) - tuhain foldert bga zuilsiig haruulna.
+// cd - change direction
+
+// var urt = +prompt("urtiig oruul");
+// var orgon = +prompt("orgoniig oruul");
+// var p = (urt + orgon) * 2;
+//  console.log(p);
+
+// var r = +prompt("radiusiig oruul");
+// var A = Math.PI * Math.pow(r, 2);
+// console.log(A);
+
+var too = +prompt("toog oruul");
+if(too % 2 == 0){
+    console.log("even");
+} else {
+    console.log("odd");
+}
---
+++ 2-8/script.js
@@ -0,0 +1,11 @@
+// Algorithm - flowchart
+// JS Basics
+// DOM - Document Object Model
+// Jquery - 
+// OOP - Object Oriented Programming
+// Async JS
+// NodeJS
+
+// Algorithm - Тодорхой үр дүнд хүрэх дарааллыг алгоритм гэж хэлнэ
+// Pseudocode - Алгоритмыг үгээр илэрхийлэх
+// Block Schema - Flowchart - Алгоритмыг схемээр илэрхийлэх
