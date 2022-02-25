6884cf575e4034f8c013fde15014a1cb8ce25a95
baigal <osmply@gmail.com>
2022-02-24 17:52:33 -0800

    2 sariin 20

--- bodloguud.js/script.js
+++ bodloguud.js/script.js
@@ -28,23 +28,75 @@
 // }
 // console.log(mobiUsers);
 
-// var x = 27;
-// var arr = [];
-// for(var i = 1; i <= x; i++){
-//     if(x % i === 0){
-//         arr.push(i);
+// 1. 100 хүртэлх тоонуудаас бүх анхны тоонуудын нийлбэрийг ол.
+// var niilber = 0;
+// for (var too = 0; too <= 100; too++) {
+//   var anhniiTooMon = true;
+//   for (var i = 2; i <= too; i++) {
+//     if (too % i === 0 && i !== too) {
+//       anhniiTooMon = false;
 //     }
+//   }
+//   if (anhniiTooMon === true) {
+//     console.log(too);
+//     niilber += too;
+//   }
 // }
-// console.log(arr);
+// console.log(niilber);
 
-// var x = "123456789";
+// 2. 100 хүртэлх тоонуудаас бүх 7-д хуваагддаг тоонуудын нийлбэр.
+// console.log("2 dahi bodlogo");
 // var sum = 0;
-// for(var i = 0; i < x.length; i++){
-//     if(+x[i] % 2 === 0){
-//         sum += +x[i];
-//     }
+// for (var too = 0; too <= 100; too++) {
+//   if (too % 7 === 0) {
+//     sum += too;
+//   }
 // }
 // console.log(sum);
+
+// 3. Гараас тоо үсэг холилдсон текст өгөгдсөн бол, тухайн текстийн
+// тоо болон үсгүүдийг салгаж , тус тусад нь стринг болгож хадгал
+// жишээ: gjskld4324jk234hj4vg23hj123 =>
+// 4324234423123 , gjskldjkhjvghj
+// console.log("3 dahi bodlogo");
+// var text = prompt("textee oruulna uu");
+// var tooSalgasan = "";
+// var textSalgasan = "";
+// for (var i = 0; i < text.length; i++) {
+//   if (
+//     text[i] == 0 ||
+//     text[i] == 1 ||
+//     text[i] == 2 ||
+//     text[i] == 3 ||
+//     text[i] == 4 ||
+//     text[i] == 5 ||
+//     text[i] == 6 ||
+//     text[i] == 7 ||
+//     text[i] == 8 ||
+//     text[i] == 9
+//   ) {
+//     tooSalgasan += tooSalgasan + text[i];
+//   } else {
+//     textSalgasan = textSalgasan + text[i];
+//   }
+// }
+// console.log(tooSalgasan);
+// console.log(textSalgasan);
+
+// 4. Гараас үг өгөгдсөн бол, тухайн үгэнд давтагдаагүй тоогоор
+// хэдэн ширхэг эгшиг үсэг байгааг тоол
+// Жишээ: presentation => e,a,i,o => 4 ширхэг эгшиг үсэг байна
+// console.log("5 dahi bodlogo");
+// function countVowels(ug) {
+//   var egshig = "aeiou";
+//   var count = 0;
+//   for (let i = 0; i < egshig.length; i++) {
+//     if (ug.toLowerCase().includes(egshig[i])) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
 
 // function bubbleSort(arr){
 //     var noChange;
@@ -85,9 +137,3 @@ function selectionSort(arr) {
     return arr;
   }
   console.log(selectionSort([56, 2, 0, 15, 6, 78, 12, 123]));
-
-// var too = prompt("toog oruul");
-// var tooHuwaasan = too.split("");
-// var tooHuwaagaadReversed = tooHuwaasan.reverse();
-// var tooHuwaagaadReversedJoin = tooHuwaagaadReversed.join("");
-// too === tooHuwaagaadReversedJoin ? console.log("palindrom too mon") : console.log("palindrom too bish");
